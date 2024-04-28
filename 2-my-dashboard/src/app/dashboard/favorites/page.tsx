a79b4cb3e53b941390e3505474ece88ed70efa7e
DanteHarold <novianrelay@gmail.com>
2024-04-27 23:50:02 -0500

    Counter-ReduxToolkit

---
+++ 2-my-dashboard/src/app/dashboard/favorites/page.tsx
@@ -0,0 +1,18 @@
+import { PokemonGrid, PokemonsReponse, SimplePokemon } from "@/pokemons";
+
+export const metadata = {
+  title: "Favoritos",
+  description: "Ad minim sit cupidatat culpa consectetur.",
+};
+
+export default async function PokemonsPage() {
+  return (
+    <div className="flex flex-col">
+      <span className="text-5xl my-2">
+        Pokemon Favoritos <small className="text-blue-800">Global State</small>
+      </span>
+
+      <PokemonGrid pokemons={[]} />
+    </div>
+  );
+}
